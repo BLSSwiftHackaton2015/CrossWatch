809b7aa64a54f5d1b9f58ed470a3f5e47f357c2f
Kacper Tłusty <kacper.tlusty@outlook.com>
2015-06-13 12:04:05 +0200

    WorkoutTableVC cells

--- CrossWatch/CrossWatch/WorkoutTableViewController.swift
+++ CrossWatch/CrossWatch/WorkoutTableViewController.swift
@@ -10,82 +10,43 @@ import UIKit
 
 class WorkoutTableViewController: UITableViewController {
 
-    var workoutArray: [Workout] = []
+    var workoutArray: NSArray?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setupWorkout()
+        
+        var timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("checkFirstRow"), userInfo: nil, repeats: true)
     }
 
     func setupWorkout() {
-        var w = Workout(name: "a", time: 0)
+
     }
     
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Potentially incomplete method implementation.
-        // Return the number of sections.
         return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete method implementation.
-        // Return the number of rows in the section.
-        return workoutArray.endIndex
+        if let count = workoutArray?.count {
+            return count
+        } else {
+            return 0
+        }
+    }
+    func checkFirstRow() {
+        self.tableView.dequeueReusableCellWithIdentifier("workoutCell", forIndexPath: <#NSIndexPath#>)
     }
 
-    /*
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as! UITableViewCell
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCellWithIdentifier("WorkoutCell", forIndexPath: indexPath) as! WorkoutTableViewCell
+        
+        if let work: Workout = workoutArray?.objectAtIndex(indexPath.row) as? Workout {
+            cell.nameWorkout.text = work.name
+            cell.timeWorkout.text = String(stringInterpolationSegment: work.time)
+        }
+    
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
